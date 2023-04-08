ceace24748e301112688d0190981b784b7a6c79d
faisalcoding8 <f4isall313@gmail.com>
2023-04-08 21:10:02 +0800

    tambah file visibility

---
+++ oop/constanst.php
@@ -0,0 +1,17 @@
+<?php 
+// define('NAMA', 'Faisal');
+// echo NAMA;
+
+// echo "<br>";
+
+// const NIP = 1234;
+// echo NIP;
+
+class Coba {
+    //define('NAMA', 'Faisal');
+    const NAMA  = 'Faisal';
+}
+
+echo Coba::NAMA;
+
+?>
